608abb7d244b50f760a9e338a412705127f12fd4
Sam <65408779+Sam5883@users.noreply.github.com>
2020-06-25 07:47:17 -0300

    Add files via upload

---
+++ Beats mod.lua
@@ -0,0 +1,60 @@
+return {
+    {
+        name = "preamp",
+        gain = 0.48875400424004,
+    },
+    {
+        name = "eq",
+        frequency = 43,
+        Q = 0.35,
+        gain = -0.353198,
+    },
+    {
+        name = "eq",
+        frequency = 1329,
+        Q = 0.88,
+        gain = -5.455788,
+    },
+    {
+        name = "eq",
+        frequency = 1826,
+        Q = 0.38,
+        gain = 4.963344,
+    },
+    {
+        name = "eq",
+        frequency = 2248,
+        Q = 2.04,
+        gain = -4.415767,
+    },
+    {
+        name = "eq",
+        frequency = 12381,
+        Q = 1.6,
+        gain = -3.679931,
+    },
+    {
+        name = "eq",
+        frequency = 2755,
+        Q = 6.26,
+        gain = -1.413957,
+    },
+    {
+        name = "eq",
+        frequency = 3727,
+        Q = 1.95,
+        gain = 0.986609,
+    },
+    {
+        name = "eq",
+        frequency = 5450,
+        Q = 3.5,
+        gain = -4.464457,
+    },
+    {
+        name = "eq",
+        frequency = 6605,
+        Q = 7.99,
+        gain = 2.3,
+    },
+}
